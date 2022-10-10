d24741087ce24545e4b739e2363214b933d28b10
DanielMarkline <592075@sga.pucminas.br>
2022-10-10 18:38:25 -0300

    beta

---
+++ BD teste/03/ORM_2/Programa.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORM_Exemplo
+{
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public ICollection<Filme> Filme { get; set; }
+
+    }
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int? GeneroId { get; set; }
+
+        [ForeignKey("GeneroId")]
+        public Genero Genero { get; set; }
+    }
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=orm;Trusted_Connection=True;");
+        }
+
+    }
+}
---
+++ BD teste/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ORM_login
+{
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public ICollection<Filme> Filme { get; set; }
+    }
+
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int GeneroId { get; set; }
+
+        [ForeignKey ("GeneroId")]
+        public Genero Genero { get; set; } 
+    }
+
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=./SQLEXPRESS;Database=orm;Trusted_Connection=True;");
+        }
+    }
+
