1ec021ed1bc37d888b9c70fcbdef50d81202afd8
DanielMarkline <592075@sga.pucminas.br>
2022-10-29 00:54:03 -0300

    CRUD - MVC e Online no SMARTERASP.NET
    Projeto de CRUD - MVC e ASP.NET. Banco de dados já online e funcional via SMARTERASP.NET.

---
+++ src/c#/PontoAll-2810/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoAll_2810.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
